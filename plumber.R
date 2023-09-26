32d3e82cb48a5f1a8147c507d5a9f56eff42e1bb
Filip Akkad <54677165+filipakkad@users.noreply.github.com>
2023-09-26 16:19:03 +0200

    Next

--- plumber.R
+++ plumber.R
@@ -31,12 +31,10 @@ list()
 
 #* @get api/list-tables
 function() {
-  # future_promise({
   withDbConnection(function(con) {
     # dbGetQuery(con, "SHOW TABLES;")$tableName
     dbListTables(con)
   })
-  #})
 }
 
 #* @post /api/data
@@ -44,43 +42,40 @@ function() {
 #* @param req JSON containing filenames
 #* @serializer unboxedJSON
 function(req) {
-  future_promise({
-    withDbConnection(function(con) {
-      table <- req$args$table
-      filters <- req$args$filters
-      page <- as.integer(req$args$page)
-      per_page <- as.integer(req$args$perPage)
-      if (is.na(page) ||
-          is.na(per_page) || page <= 0 || per_page <= 0) {
-        res$status <- 400
-        return(
-          list(error = "Invalid parameters. Please provide valid page and per_page values.")
-        )
-      }
+  withDbConnection(function(con) {
+    table <- req$args$table
+    filters <- req$args$filters
+    page <- as.integer(req$args$page)
+    per_page <- as.integer(req$args$perPage)
+    if (is.na(page) ||
+        is.na(per_page) || page <= 0 || per_page <= 0) {
+      res$status <- 400
+      return(list(error = "Invalid parameters. Please provide valid page and per_page values."))
+    }
 
-      withDbConnection(function(serverCon) {
-        localTables <- DBI::dbListTables(con)
-        if (!(table %in% localTables)) {
-          print("Copying")
-          copy_to(con,
-                  tbl(serverCon, table) %>% collect(),
-                  table,
-                  temporary = FALSE)
-        }
-      })
-      queries <- buildQuery(table, filters, page, per_page, con)
-      collectedData <- dbGetQuery(con, queries$paginated_dt)
-      result = list(
-        data = collectedData,
-        count = queries$totalCount,
-        columns = names(collectedData) %>% purrr::map(function(name) {
-          list(id = name,
-               name = name)
-        })
-      )
-      result
+    withDbConnection(function(serverCon) {
+      localTables <- DBI::dbListTables(con)
+      if (!(table %in% localTables)) {
+        print("Copying")
+        copy_to(con,
+                tbl(serverCon, table) %>% collect(),
+                table,
+                temporary = FALSE)
+      }
     })
+    queries <- buildQuery(table, filters, page, per_page, con)
+    collectedData <- dbGetQuery(con, queries$paginated_dt)
+    result = list(
+      data = collectedData,
+      count = queries$totalCount,
+      columns = names(collectedData) %>% purrr::map(function(name) {
+        list(id = name,
+             name = name)
+      })
+    )
+    result
   })
+
 }
 
 
@@ -90,13 +85,12 @@ function(req, res) {
   filters <- req$args$params$filters
   filename <- file.path(tempdir(), glue::glue("{table}.csv"))
 
-  future_promise({
-    withDbConnection(function(con) {
-      query <- buildQuery(table, filters, con = con)
-      dbGetQuery(con, query$unpaginated_tbl)
-    })
-  }) %...>% (function(dt) {
-    write.csv(dt, filename, row.names = FALSE)
-    include_file(filename, res, "text/csv")
+  dt <- withDbConnection(function(con) {
+    query <- buildQuery(table, filters, con = con)
+    dbGetQuery(con, query$unpaginated_tbl)
   })
+
+  write.csv(dt, filename, row.names = FALSE)
+  include_file(filename, res, "text/csv")
+
 }
